ffc6913be7a5a8615bcae47c8750b88aea987b10
alluuzx <95376116+alluuzx@users.noreply.github.com>
2023-11-18 18:10:41 +0200

    Add files via upload

---
+++ fnres/fnres/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using fnres.classes;
+using System;
+using System.Collections;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace fnres
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //get values
+                int width = int.Parse(WidthBox.Text);
+                int height = int.Parse(HeightBox.Text);
+                int mode = ModeBox.SelectedIndex;
+                int fps = int.Parse(FPSBox.Text);
+                bool llm = (bool)LowLatencyMode.IsChecked;
+
+                //check if file is read only and disable it during change
+                FileInfo file = new FileInfo(GameUserSettings.GameUserSettingsFile);
+
+                //check if it even exists
+                if (!file.Exists)
+                {
+                    throw new Exception("Settings file doesn't exist!");
+                }
+
+                //set read only to off
+                file.IsReadOnly = false;
+
+                //write config
+                GameUserSettings.SetConfig(width, height, fps, mode, llm);
+
+                //set read only back to enabled
+                file.IsReadOnly = true;
+
+                MessageBox.Show("Succesfully applied settings to " + file.FullName, "fnres", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "fnres", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //get values to show
+            try
+            {
+                ArrayList values = GameUserSettings.GetConfig();
+
+                WidthBox.Text = values[0].ToString();
+                HeightBox.Text = values[1].ToString();
+                FPSBox.Text = values[2].ToString();
+                ModeBox.SelectedIndex = (int)values[3];
+                LowLatencyMode.IsChecked = (bool)values[4];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while getting GameUserSettings values, is the file corrupted? Message: " + ex.Message, "fnres", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ fnres/fnres/classes/GameUserSettings.cs
@@ -0,0 +1,95 @@
+﻿using IniParser;
+using IniParser.Model;
+using System;
+using System.Collections;
+
+namespace fnres.classes
+{
+    public static class GameUserSettings
+    {
+        public static string GameUserSettingsFile { get; } = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/FortniteGame/Saved/Config/WindowsClient/GameUserSettings.ini";
+        private static string Section { get; } = "/Script/FortniteGame.FortGameUserSettings";
+
+        private static readonly FileIniDataParser parser = new FileIniDataParser();
+
+        private static string GetValue(string key, IniData data)
+        {
+            return data[Section][key];
+        }
+
+        private static void SetValue<T>(string key, T value, IniData data)
+        {
+            data[Section][key] = value.ToString();
+        }
+
+        /// <summary>
+        /// Get the config file as an array, order is the same as in SetConfig()
+        /// </summary>
+        /// <returns>Array containing the values of the GameUserSettings ini file</returns>
+        public static ArrayList GetConfig()
+        {
+            //configuration
+            parser.Parser.Configuration.AssigmentSpacer = string.Empty;
+
+            //read file
+            IniData data = parser.ReadFile(GameUserSettingsFile);
+
+            //assign
+            int width = int.Parse(GetValue("ResolutionSizeX", data));
+            int height = int.Parse(GetValue("ResolutionSizeY", data));
+            int fps = int.Parse(GetValue("FrameRateLimit", data));
+            int mode = int.Parse(GetValue("FullscreenMode", data));
+            bool llm = bool.Parse(GetValue("LowInputLatencyModeIsEnabled", data));
+
+            //return values
+            return new ArrayList
+            {
+                width, height, fps, mode, llm
+            };
+        }
+
+        /// <summary>
+        /// Sets the fortnite configuration
+        /// </summary>
+        /// <param name="width">resolution x</param>
+        /// <param name="height">resolution y</param>
+        /// <param name="fps">fps cap</param>
+        /// <param name="mode">fullscreen mode</param>
+        /// <param name="llm">low latency mode</param>
+        public static void SetConfig(int width, int height, int fps, int mode, bool llm)
+        {
+            //configuration
+            parser.Parser.Configuration.AssigmentSpacer = string.Empty;
+
+            //read file
+            IniData data = parser.ReadFile(GameUserSettingsFile);
+
+            //width
+            SetValue("ResolutionSizeX", width, data);
+            SetValue("LastUserConfirmedResolutionSizeX", width, data);
+            SetValue("DesiredScreenWidth", width, data);
+            SetValue("LastUserConfirmedDesiredScreenWidth", width, data);
+
+            //height
+            SetValue("ResolutionSizeY", height, data);
+            SetValue("LastUserConfirmedResolutionSizeY", height, data);
+            SetValue("DesiredScreenHeight", height, data);
+            SetValue("LastUserConfirmedDesiredScreenHeight", height, data);
+
+            //fps
+            SetValue("FrameRateLimit", fps, data);
+            SetValue("FrontendFrameRateLimit", fps, data);
+
+            //mode
+            SetValue("LastConfirmedFullscreenMode", mode, data);
+            SetValue("PreferredFullscreenMode", mode, data);
+            SetValue("FullscreenMode", mode, data);
+
+            //llm
+            SetValue("LowInputLatencyModeIsEnabled", llm, data);
+
+            //save
+            parser.WriteFile(GameUserSettingsFile, data);
+        }
+    }
+}
